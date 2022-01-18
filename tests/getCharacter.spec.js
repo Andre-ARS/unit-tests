9c45cc678391faa85bfb23ac1075b6d3a05ab89c
André Rodrigues <andre360.ars@gmail.com>
2022-01-18 10:52:09 -0300

    Requisito 9 - testa se os paramentros são Case Sensitive

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -54,13 +54,13 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função retorna o objeto correto para o parâmetro "Melissandre"', () => {
     expect(getCharacter('Melissandre')).toEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'], });
   });
-  // it('Verifica se os parâmetros não são Case Sensitive', () => {
-  //   expect(getCharacter('MelIsSanDrE')).toEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'], });
-  //   expect(getCharacter('bRIenNe')).toEqual({ name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'], });
-  //   expect(getCharacter('ARyA')).toEqual({ name: 'Arya Stark', class: 'Rogue', phrases: [ 'Not today', 'A girl has no name.' ] });
-  //   expect(getCharacter('BRIENNE')).toEqual({ name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'], });
-  //   expect(getCharacter('melissandre')).toEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'], });
-  // });
+  it('Verifica se os parâmetros não são Case Sensitive', () => {
+    expect(getCharacter('MelIsSanDrE')).toEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'], });
+    expect(getCharacter('bRIenNe')).toEqual({ name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'], });
+    expect(getCharacter('ARyA')).toEqual({ name: 'Arya Stark', class: 'Rogue', phrases: [ 'Not today', 'A girl has no name.' ] });
+    expect(getCharacter('BRIENNE')).toEqual({ name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'], });
+    expect(getCharacter('melissandre')).toEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'], });
+  });
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
