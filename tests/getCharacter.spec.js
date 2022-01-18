3ae7b50d5e2410c22911f2032264028228cb76b8
André Rodrigues <andre360.ars@gmail.com>
2022-01-18 10:50:54 -0300

    Requisito 9 - testa se a função retorna o objeto correto, caso o parametro seja "Melissandre"

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -51,9 +51,9 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função retorna o objeto correto para o parâmetro "Brienne"', () => {
     expect(getCharacter('Brienne')).toEqual({ name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'], });
   });
-  // it('Verifica se a função retorna o objeto correto para o parâmetro "Melissandre"', () => {
-  //   expect(getCharacter('Melissandre')).toEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'], });
-  // });
+  it('Verifica se a função retorna o objeto correto para o parâmetro "Melissandre"', () => {
+    expect(getCharacter('Melissandre')).toEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'], });
+  });
   // it('Verifica se os parâmetros não são Case Sensitive', () => {
   //   expect(getCharacter('MelIsSanDrE')).toEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'], });
   //   expect(getCharacter('bRIenNe')).toEqual({ name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'], });
